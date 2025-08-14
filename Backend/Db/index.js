adfb4c3971420c97f8e9b37294cdc7a58816d12f
Muhammad_Siddiqui <muhammadsiddiqui1410@gmail.com>
2025-08-14 01:19:36 -0700

    implement chalking on console

--- Backend/Db/index.js
+++ Backend/Db/index.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
+import chalk from "chalk";
 
 
 mongoose.connect('mongodb+srv://muhammadsiddiqui1410:ms12345@cluster0.rdqtavi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-    .then(() => console.log(" MongoDB connected successfully"))
-    .catch(err => console.error(" MongoDB connection error:", err));
+    .then(() => console.log(chalk.bgGreen.white(" MongoDB connected successfully")))
+    .catch(err => console.error(chalk.red(" MongoDB connection error:"), err));
 
 export default mongoose;
--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,8 @@ import cors from 'cors';
 import router from "./routers/index.js";
 import dotenv from 'dotenv';
 import session from 'express-session';
-import mongoose from './Db/index.js'
+import mongoose from './Db/index.js';
+import chalk from 'chalk';
 
 
 dotenv.config();
@@ -12,9 +13,9 @@ const app = express()
 
 const db = mongoose.connection;
 
-db.on("error ", console.error.bind(console, " Connection error:"));
+db.on("error ", console.error.bind(console, chalk.bgRed(" Connection error:")));
 db.once("open", function () {
-    console.log(" DB connected!");
+    console.log(chalk.bgWhite.black(" DB connected!"));
 });
 
 app.use(
@@ -33,5 +34,5 @@ app.use(cors())
 app.use('/api', router)
 
 app.listen('4000', () => {
-    console.log(`Server is running on port ${4000}`);
+    console.log(chalk.bgBlue.white(`Server is running on port ${4000}`));
 })
